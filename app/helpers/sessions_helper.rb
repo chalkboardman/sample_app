9e39e821dfdefb1bb1aeef8a8e2d1c556eaff23c
Shawn R. McDonald <shawn@chalkboardmanifesto.com>
2010-06-16 16:15:13 -0700

    Added signing in stuff.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,17 @@ module SessionsHelper
                                   :expires => 20.years.from_now.utc }
     self.current_user = user
   end
+  
+  def current_user=(user)
+    @current_user ||= user_from_remember_token
+  end
+  
+  def user_from_remember_token
+    remember_token = cookie[:remember_token]
+    User.find_by_remember_token(remember_token) unless remember_token.nil?
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
 end
