d40e74f8003db33811579fb6d4fe250b098f43f9
Shawn R. McDonald <shawn@chalkboardmanifesto.com>
2010-06-16 15:53:39 -0700

    Full create function in controller, doesn't work yet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,9 @@ class SessionsController < ApplicationController
       render 'new'
     else
       #Sign in the user and redirect to the user's show page
+      #NOT YET WORKING
+      sign_in user
+      redirect_to user
     end
   end
   
