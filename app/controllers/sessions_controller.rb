cfd73398c01e152312601244123cef5a8c773753
Shawn R. McDonald <shawn@chalkboardmanifesto.com>
2010-06-16 15:30:17 -0700

    Added fail condition for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.authenticate(params[:session][:email], 
+                             params[:session][:password])
+    if user.nil?
+      #Create an error message and re-render the signin form.
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      #Sign in the user and redirect to the user's show page
+    end
   end
   
   def destroy
