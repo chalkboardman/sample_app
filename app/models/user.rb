91dc3889e35dcc3773c0f80226a6a7c33d2f3a7d
Shawn R. McDonald <shawn@chalkboardmanifesto.com>
2010-06-16 08:01:16 -0700

    Added some password stuff, still need to add encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -7,4 +8,22 @@ class User < ActiveRecord::Base
   validates_length_of :name, :maximum => 50
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+  
+  #Automatically create the virtual attribute 'password_confirmation'
+  validates_confirmation_of :password
+  
+  #Password validations
+  validates_presence_of :password
+  validates_length_of :password, :within => 6..40
+  
+  before_save :encrypt_assword
+  
+  private
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      string # Only a temporary implementation
+    end
 end
