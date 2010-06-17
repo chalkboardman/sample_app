61b815cc0df948a000aeebff16cd119828f2f480
Shawn R. McDonald <shawnrmcdonald@gmail.com>
2010-06-17 14:23:04 -0700

    User can only edit own profile page, not others

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,4 +27,13 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
+  
+  def deny_access
+    flash[:notice] = "Please sign in to access this page."
+    redirect_to signin_path
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
 end
