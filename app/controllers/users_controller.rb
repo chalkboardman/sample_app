53353a845a576ae3db17ee9647bfcd5029b3b61b
Shawn R. McDonald <shawnrmcdonald@gmail.com>
2010-06-18 10:28:08 -0700

    can't access new/create if signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user, :only => :destroy
+  before_filter :not_signed_in, :only => [:new, :create]
   
   def new
     @user = User.new
@@ -65,6 +66,10 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def not_signed_in
+      redirect_to(root_path) if signed_in?
     end
 
 end
