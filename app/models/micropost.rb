bf63184e366ae2897c9b2570735e509395486b22
Shawn R. McDonald <shawnrmcdonald@gmail.com>
2010-06-18 15:46:06 -0700

    Added associations btwn users and microposts, added default_scope for microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ActiveRecord::Base
+  attr_accessible :content
+  
+  belongs_to :user
+  
+  default_scope :order => 'created_at DESC'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   
+  has_many :microposts
+  
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates_presence_of :name, :email
