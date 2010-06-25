3b6c86ec0058018219401a1409451ebced4e28ef
Shawn R. McDonald <shawnrmcdonald@gmail.com>
2010-06-25 09:28:50 -0700

    added delete link to microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   before_filter :authenticate
   
   def create
-    @micropost = current.microposts.build(params[:micropost])
+    @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost saved!"
       redirect_to root_path
